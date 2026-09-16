aa256cfd1bcc99d73b1f2e7b9df13187f1cbbf76
9529360-cpu <9529360@gmail.com>
2026-09-16 23:10:09 +0200

    refactor(desktop): centralize Stardust product identity

---
+++ apps/desktop/src/product-brand.ts
@@ -0,0 +1,3 @@
+export const PRODUCT_NAME = 'Stardust' as const
+export const PRODUCT_DESKTOP_NAME = `${PRODUCT_NAME} Desktop` as const
+export const PRODUCT_REPOSITORY_URL = 'https://github.com/9529360-cpu/stardust-hermes' as const
