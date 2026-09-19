4d67fe2d197d5532f5b699601950cdfeb0ab9535
9529360-cpu <9529360@gmail.com>
2026-09-19 08:06:28 +0200

    test(update): keep admission coverage behind private engine

--- tests/hermes_cli/test_cmd_update_docker.py
+++ tests/hermes_cli/test_cmd_update_docker.py
@@ -21,7 +21,7 @@ from unittest.mock import patch
 
 import pytest
 
-from hermes_cli.main import cmd_update
+from hermes_cli.main import _run_update_transaction as cmd_update
 from hermes_cli.update_cmd import _cmd_update_check
 
 
